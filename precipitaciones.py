545ef8dc61d357a795fad19e98657f47dca6d764
JuanRuizIng <juan_andres.ruiz@uao.edu.co>
2024-06-14 20:25:25 -0500

    precipitaciones v.2
    Se incluye de manera general el código. Se debe ejecutar para generar el DataFrame debido al tamaño. Se incluyen logos y el dashboard

---
+++ precipitaciones.py
@@ -0,0 +1,54 @@
+import requests
+import pandas as pd
+import io
+import csv
+
+url = "https://www.datos.gov.co/resource/s54a-sgyg.csv?$query=SELECT%20%60codigoestacion%60,%20%60codigosensor%60,%20%60fechaobservacion%60,%20%60valorobservado%60,%20%60nombreestacion%60,%20%60departamento%60,%20%60municipio%60,%20%60zonahidrografica%60,%20%60latitud%60,%20%60longitud%60,%20%60descripcionsensor%60,%20%60unidadmedida%60%20WHERE%20caseless_one_of(%60departamento%60,%22VALLE%20DEL%20CAUCA%22,%22CHOC%C3%93%22,%22CHOCO%22,%22NARI%C3%91O%22,%22NARINO%22,%22CAUCA%22)%20LIMIT%2035000000%20OFFSET%200&$$app_token=7DJ4ddS576T0c1MrFzJqNLP2u"
+
+response = requests.get(url)
+
+data = io.StringIO(response.text)
+dialect = csv.Sniffer().sniff(data.read(1024))
+data.seek(0)
+df = pd.read_csv(data, delimiter=dialect.delimiter)
+print(df.head(-1))
+print(len(df))
+
+# Imprime las primeras 3 filas del DataFrame
+print(df.head(3))
+print(len(df))
+
+datos_vacios = df.isnull().sum(axis=1).sum()
+#print(datos_vacios)
+if datos_vacios > 0:
+    df = df.dropna()
+
+df = df.drop(['codigoestacion', 'codigosensor', 'descripcionsensor', 'unidadmedida'], axis=1)
+
+df['departamento'] = df['departamento'].replace({'NARINO': 'NARIÑO', 'CHOCO': 'CHOCÓ'})
+
+print(df['departamento'].unique())
+print(len(df))
+print(df.head(3))
+
+# Convierte la columna 'fechaobservacion' a datetime
+df['fechaobservacion'] = pd.to_datetime(df['fechaobservacion'])
+
+# Crea nuevas columnas para el año, mes, día y hora
+df['año'] = df['fechaobservacion'].dt.year
+df['mes'] = df['fechaobservacion'].dt.month
+df['día'] = df['fechaobservacion'].dt.day
+df['hora'] = df['fechaobservacion'].dt.hour
+
+print(df["día"])
+
+print(f"El número de filas es {df.shape[0]}")
+
+df = df.reset_index(drop=True)
+
+df = df.drop(['fechaobservacion'], axis=1)
+
+print(df.head(5))
+
+# Guarda el DataFrame como un archivo CSV
+df.to_csv('mi_dataframe_API.csv', index=False)
